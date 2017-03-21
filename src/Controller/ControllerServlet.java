81f23824aba85cf43013112709c521d9094b4f60
atepaevm <atepaev_mihail@mail.ru>
2017-03-21 22:16:29 +0300

    Final

--- src/Controller/ControllerServlet.java
+++ src/Controller/ControllerServlet.java
@@ -13,6 +13,29 @@ import java.io.IOException;
 @WebServlet(name = "ControllerServlet", urlPatterns = "/index")
 public class ControllerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String xString=request.getParameter("x_coord");
+        String yString=request.getParameter("y_coord");
+        String RString=request.getParameter("chBox");
+        int x=1000;
+        int y=1000
+        int R=1000;
+        boolean isWrong=false;
+        try{
+            x=Integer.parseInt(xString);
+            y=Integer.parseInt(yString);
+            R=Integer.parseInt(RString);
+        }
+        catch (NumberFormatException e){
+            isWrong=true;
+        }
+        if(!isWrong){
+            if(x<-3||x>5||y<-5||y>3){
+                isWrong=true;
+            }
+        }
+        if(!isWrong){
+
+        }
         //request.getRequestDispatcher("/WEB-INF/checking.jsp").forward(request,response);
     }
 
